09285d23a00f190d5b8bdbb70f778d16b51aaaec
FILIPE <filipelao1003@gmail.com>
2017-01-26 16:39:05 +0800

    Initial Project

---
+++ ImagsaSDK/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ImagsaSDK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblLicense = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(52, 50);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(61, 13);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "Disconnect";
+            // 
+            // lblLicense
+            // 
+            this.lblLicense.AutoSize = true;
+            this.lblLicense.Location = new System.Drawing.Point(52, 168);
+            this.lblLicense.Name = "lblLicense";
+            this.lblLicense.Size = new System.Drawing.Size(71, 13);
+            this.lblLicense.TabIndex = 1;
+            this.lblLicense.Text = "License Plate";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(247, 168);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(35, 13);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 366);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblLicense);
+            this.Controls.Add(this.lblStatus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblLicense;
+        private System.Windows.Forms.Label lblTime;
+    }
+}
+
---
+++ ImagsaSDK/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Imagsa.Chronos;
+using System;
+using System.Runtime.InteropServices;
+using ImagsaSDK.JavaServices;
+
+namespace ImagsaSDK
+{
+    public partial class Form1 : Form
+    {
+        public ChronosSDK chronosSDK;
+        private List<Event> liveEvents = new List<Event>();
+        private List<Event> liveEventsFiltered = new List<Event>();
+        public JavaServices.CarEnterClient Client = new CarEnterClient();
+       
+        private const int MAX_LIVE_EVENTS = 1000;
+
+        // Live viewer
+        private System.Timers.Timer timer;
+        private int lastId;
+        private Object retrievingLastEventLock = new Object();
+        private bool retrievingLastEvent = false;
+        public Form1()
+        {
+            InitializeComponent();
+            //chronosSDK
+            chronosSDK = new ChronosSDK();
+            if (connect())
+            {
+                
+            }
+        }
+
+        private bool connect()
+        {
+            try
+            {
+                chronosSDK.Connect("192.168.1.110", Convert.ToInt32("4359"), false);
+            }
+            catch (System.Exception ex)
+            {
+                return false;
+            }
+            lblStatus.Text = "Connected";
+
+            // Start the timer
+            timer = new System.Timers.Timer(1000);
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
+            timer.Start();
+
+            return true;
+        }
+
+        private void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            // Check if currently retrieving last event
+            lock (retrievingLastEventLock)
+            {
+                if (retrievingLastEvent) return;
+                retrievingLastEvent = true;
+            }
+
+            // Check not initial event
+            try
+            {
+                // Is there any new event?
+                int id = chronosSDK.GetLatest();
+
+                if (lastId == 0) lastId = id;
+
+                while (id > lastId)
+                {
+                    // Check no more than 100
+                    if (id > lastId + 100) id = lastId + 100;
+
+                    // Get the event
+                    //List<Event> evts = chronosSDK.GetEvents(lastId + 1, id);
+                    //evts.Reverse();
+
+                    List<Event> evts = chronosSDK.GetEvents(lastId + 1, id);
+                    evts.Reverse();
+                    Event test = evts[0];
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        string PlateResult = "";
+                           
+                        if (test.OCR_Plate == "UNKNOWN")
+                        {
+                            //PlateResult = "MM1234";
+                            lblLicense.Text = PlateResult;
+                            lblTime.Text = DateTime.Now.ToLongTimeString();
+                        }
+                        else
+                        {
+                            PlateResult = test.OCR_Plate;
+                            lblLicense.Text = PlateResult; 
+                        }
+                        Client.getVehicleNo(PlateResult);
+                       
+                    });
+                    lastId = id;
+                }     
+            }
+            catch (ChronosException ex)
+            {
+            }
+
+            lock (retrievingLastEventLock)
+            {
+                retrievingLastEvent = false;
+            }
+        }
+    }
+}
